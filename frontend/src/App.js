890dd5ffda7d768311fd3b3bff6a0dc41c822d2b
Max Forssén <max.forssen@mentimeter.com>
2021-01-26 11:48:39 +0100

    fetch backend data to front end. display snippet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import parse from 'html-react-parser'
 
 export const App = () => {
+
+  const [checkout, setCheckout] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:8080/checkout')
+      .then((res) => {
+        if (!res.ok) {
+          throw "Fetch checkout failed";
+        }
+        return res.json();
+      })
+      .then((json) => {
+        setCheckout(json);
+        console.log(json)
+      })
+      .catch((error) => console.error(error))
+
+  };
+
   return (
     <div>
-      Find me in src/app.js!
+      <button type="submit" onClick={handleSubmit}>Fetch checkout</button>
+      {checkout && parse(checkout.snippet.snippet)} 
+      
     </div>
   )
 }
