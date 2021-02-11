e1921910e71f22f735b881f8abafa4e99a86022b
monkeybusiness101 <max.forssen@mentimeter.com>
2021-02-11 16:10:45 +0100

    env variables

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import fetch from 'node-fetch'
-import crypto from "crypto";
-import bcrypt from "bcrypt";
+import crypto from "crypto"
+import bcrypt from "bcrypt"
 
 dotenv.config()
 
@@ -76,20 +76,20 @@ const User = mongoose.model("User", {
     default: () => crypto.randomBytes(128).toString("hex"),
     unique: true
   }
-});
+})
 
 const authenticateUser = async (req, res, next) => {
   try {
-    const user = await User.findOne({ accessToken: req.header("Authorization") });
+    const user = await User.findOne({ accessToken: req.header("Authorization") })
     if (!user) {
       throw "User not found";
     }
-    req.user = user;
-    next();
+    req.user = user
+    next()
   } catch (err) {
-    res.status(401).json({ loggedOut: true });
+    res.status(401).json({ loggedOut: true })
   }
-};
+}
 
 // Create user
 app.post("/users", async (req, res) => {
@@ -105,7 +105,7 @@ app.post("/users", async (req, res) => {
     const zip = req.body.zip
     const city = req.body.city
     const cellno = req.body.cellno
-    const salt = bcrypt.genSaltSync();
+    const salt = bcrypt.genSaltSync()
     const user = await new User({
       email,
       orgnr,
@@ -117,28 +117,28 @@ app.post("/users", async (req, res) => {
       city,
       cellno,
       password: bcrypt.hashSync(password, salt)
-    }).save();
-    res.status(200).json({ userId: user._id, accessToken: user.accessToken });
+    }).save()
+    res.status(200).json({ userId: user._id, accessToken: user.accessToken })
   } catch (err) {
-    res.status(400).json({ message: "Could not create user", errors: err });
+    res.status(400).json({ message: "Could not create user", errors: err })
   };
 });
 
 // Login
 app.post("/sessions", async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
+  const user = await User.findOne({ email: req.body.email })
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
-    res.json({ userId: user._id, accessToken: user.accessToken });
+    res.json({ userId: user._id, accessToken: user.accessToken })
   } else {
-    res.json({ notFound: true });
-  };
+    res.json({ notFound: true })
+  }
 })
 
 // Authenticated endpoint to retrieve user profile data
-app.get("/profile", authenticateUser);
+app.get("/profile", authenticateUser)
 app.get("/profile", async (req, res) => {
   if (req.header("userId") != req.user._id) {
-    res.status(403).json({ error: "Access Denied" });
+    res.status(403).json({ error: "Access Denied" })
   } else {
     res.status(200).json({
       userId: req.user._id, 
@@ -152,9 +152,9 @@ app.get("/profile", async (req, res) => {
       city: req.user.city,
       cellNo: req.user.cellno,
       orgNo: req.user.orgnr
-    });
-  };
-});
+    })
+  }
+})
 
 //GET session token
 const sessionToken = async () => {
@@ -166,7 +166,6 @@ const sessionToken = async () => {
   })
   .then(response => response.json())
   .then(token => {
-    console.log(token, "initial call")
     return token.token
   }).catch((err) => console.log(err))
 }
@@ -174,7 +173,6 @@ const sessionToken = async () => {
 //POST session
 const checkout = async (token, input) => {
   const bearer = token
-  console.log(bearer)
   return await fetch('https://playground-api.briqpay.com/checkout/v1/sessions', {
     method: 'POST',
     body: JSON.stringify(input),
@@ -185,7 +183,6 @@ const checkout = async (token, input) => {
   })
   .then(response => response.json())
   .then(session => {
-    console.log(session, "second call")
     return { snippet: session.snippet, newToken: session.token }
   }).catch((err) => console.log(err))
 }
@@ -193,7 +190,6 @@ const checkout = async (token, input) => {
 app.post('/checkout', async(req, res) => {
   try {
     const input = req.body
-    console.log(JSON.stringify(input), "backend input")
     const token = await sessionToken()
     const snippet = await checkout(token, input)
 
